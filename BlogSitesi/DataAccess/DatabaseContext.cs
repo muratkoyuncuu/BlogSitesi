3d685e6be4cbb4e7005cf2b6ab52b92a60f4f487
muratkoyuncuu <muratkoyuncu@outlook.com.tr>
2023-02-02 12:07:00 +0300

    Add project files.

---
+++ BlogSitesi/DataAccess/DatabaseContext.cs
@@ -0,0 +1,44 @@
+﻿using BlogSitesi.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace BlogSitesi.DataAccess
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Post>? Posts { get; set; }
+        public DbSet<User>? Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Genel",
+                    IsActive = true
+                });
+
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    IsActive = true,
+                    IsAdmin= true,
+                    Name="Admin",
+                   SurName="User",
+                   Email="admin@blogsitesi.com",
+                   Password="123",
+                   UserName="Admin"
+
+                }) ;
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; database=BlogSitesi; integrated security=true");
+            base.OnConfiguring(optionsBuilder); 
+        }
+    }
+}
---
+++ BlogSitesi/ViewComponents/Categories.cs
@@ -0,0 +1,20 @@
+﻿using BlogSitesi.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSitesi.ViewComponents
+{
+    public class Categories : ViewComponent
+    {
+        private readonly DatabaseContext _database;
+
+        public Categories(DatabaseContext database)
+        {
+            _database = database;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _database.Categories.ToListAsync());
+        }
+    }
+}
